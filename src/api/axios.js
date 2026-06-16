2a1af3d5a90df2e6869af7afbe9a72459514d88c
Samah Salah <samahsalah2555@gmail.com>
2026-06-16 13:02:48 +0300

    change axois

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://residence-married-curriculum-disclaimers.trycloudflare.com/api/v1",
+  baseURL: "https://sailing-angels-fabric-knights.trycloudflare.com/api/v1",
   withCredentials: true,
   xsrfCookieName: "session_id",
   xsrfHeaderName: "X-Session-Id",

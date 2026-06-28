c19180efffd23d57301168a819fdd76f51f5adba
Samah Salah <samahsalah2555@gmail.com>
2026-06-28 15:58:49 +0300

    add endservice2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://korea-feed-government-robertson.trycloudflare.com/api/v1",
+  baseURL: "https://significantly-advertising-benefit-prisoner.trycloudflare.com/api/v1",
   withCredentials: true,
   xsrfCookieName: "session_id",
   xsrfHeaderName: "X-Session-Id",

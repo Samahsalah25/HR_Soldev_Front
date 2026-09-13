fcf0914fab2a5042cc08df6a6e676b211e503ff2
Samah Salah <samahsalah2555@gmail.com>
2026-09-13 17:26:46 +0300

    add sales29

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -26,7 +26,7 @@ import {
   confirmPurchase,
   cancelPurchase,
   createBillFromPurchase,
-} from "../api/PurchasesApi";
+} from "../api/Purchasesapi";
 import { extractApiErrorMessage } from "@/lib/apiErrors";
 import { useConfirm } from "@/components/ui/confirm-dialog";
 import { useToast } from "@/components/ui/use-toast";
